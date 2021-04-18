386108673c9eca830fc33348cdbaca75ec47c071
kult0922 <38486600+kult0922@users.noreply.github.com>
2021-04-18 09:44:22 +0900

    タブボタンのデザイン変更

--- src/components/layouts/index.tsx
+++ src/components/layouts/index.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import { CSSProperties } from 'react';
 import { Link } from 'react-router-dom';
 import { Header } from '../organisms/Header';
 
@@ -6,19 +7,57 @@ const Wrapper = styled.div({
   textAlign: 'center',
 });
 
+type underLineProps = {
+  bottom: CSSProperties['bottom'];
+};
+
+const StyledLink = styled(Link)<underLineProps>`
+  position: relative;
+  display: inline-block;
+  color: #1b1b1b;
+  text-decoration: none;
+  width: 60px;
+  padding-right: 20px;
+  padding-left: 20px;
+  padding-top: 10px;
+  &:hover {
+    cursor: pointer;
+    text-decoration: none;
+  }
+  &::after {
+    position: absolute;
+    bottom: -4px;
+    bottom: ${(props) => props.bottom};
+    left: 10%;
+    content: '';
+    width: 80%;
+    height: 2px;
+    background: #333;
+    transform: scale(0, 1);
+    transform-origin: right top;
+    transition: transform 0.2s;
+  }
+  &:hover::after {
+    transform-origin: left top;
+    transform: scale(1, 1);
+  }
+`;
+
 export const Template: React.FC = ({ children }) => {
   return (
     <Wrapper>
-      <Header>栗崎園</Header>
-      <Link to="/">
-        <button>ホーム</button>
-      </Link>
-      <Link to="/order">
-        <button>注文</button>
-      </Link>
-      <Link to="/cart">
-        <button>カート</button>
-      </Link>
+      <Header>栗崎園 </Header>
+
+      <StyledLink bottom="-4px" to="/">
+        ホーム
+      </StyledLink>
+      <StyledLink bottom="-4px" to="/order">
+        注文
+      </StyledLink>
+      <StyledLink bottom="-7px" to="/cart">
+        <img src="/shopping-cart.svg" width="18" height="18"></img>
+      </StyledLink>
+
       {children}
     </Wrapper>
   );
