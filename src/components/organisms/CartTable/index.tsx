fc335d04ae9a51a400e2a2e3d9aba93078d62e58
kult0922 <38486600+kult0922@users.noreply.github.com>
2021-12-12 22:18:24 +0900

    カートテーブルのレスポンシブ対応

--- src/components/organisms/CartTable/index.tsx
+++ src/components/organisms/CartTable/index.tsx
@@ -1,6 +1,6 @@
 import { GlobalStore } from '../../../store/Global';
 import styled from '@emotion/styled';
-import { Flex } from '../../../lib/styled';
+import { bp } from '../../../constants/css';
 
 export const Table = styled.table({
   border: 'solid 2px #aaaaaa',
@@ -26,7 +26,10 @@ const ItemData = styled.td({
 
 const TotalData = styled.td({
   width: '5%',
-  fontSize: '1.5em',
+  fontSize: '24px',
+  [bp.md]: {
+    fontSize: '18px',
+  },
 });
 
 const DeleteButton = styled.div({
@@ -43,63 +46,66 @@ export const CartTable: React.FC<Props> = ({ editable, showTotal }) => {
   const { carts, total, onDeleteCartItem, onChangeCartItemAmount } = cartStore;
   if (!carts.length) return <div>ショッピングカートに商品は入っていません。</div>;
   return (
-    <Table>
-      <tbody>
-        <tr>
-          <th>商品名</th>
-          <th></th>
-          <th>値段</th>
-          <th>個数</th>
-          <th>小計</th>
-        </tr>
-        {carts.map((item, i) => {
-          return (
-            <ItemRow key={i}>
-              <ItemData>
-                <img src={item.imagePaths[0]} width="100px" alt="itme" />
-              </ItemData>
-              <ItemData>{item.name}</ItemData>
-              <ItemData>{item.price}円</ItemData>
-              <ItemData>
-                {editable ? (
-                  <Flex justifyContent="space-between" m={'0 25px'}>
-                    <select
-                      defaultValue={item.amount}
-                      onChange={(event) => {
-                        onChangeCartItemAmount(item.id, Number(event.target.value));
-                      }}
-                    >
-                      {[...Array(10)].map((_, i) => (
-                        <option key={i} value={i + 1}>
-                          {i + 1}個
-                        </option>
-                      ))}
-                    </select>
-                    <DeleteButton
-                      onClick={() => {
-                        onDeleteCartItem(item.id);
-                      }}
-                    >
-                      削除
-                    </DeleteButton>
-                  </Flex>
-                ) : (
-                  item.amount
-                )}
-              </ItemData>
-              <ItemData>{item.price * item.amount}円</ItemData>
-            </ItemRow>
-          );
-        })}
-        {showTotal ? (
+    <div>
+      <Table>
+        <tbody>
           <tr>
-            <td colSpan={4}></td>
-            <TotalData>合計: {total}円</TotalData>
+            <th></th>
+            <th>商品名</th>
+            <th>値段 個数</th>
+            <th>小計</th>
           </tr>
-        ) : (
-          <tr />
-        )}
-      </tbody>
-    </Table>
+          {carts.map((item, i) => {
+            return (
+              <ItemRow key={i}>
+                <ItemData>
+                  <img src={item.imagePaths[0]} width="100px" alt="itme" />
+                </ItemData>
+                <ItemData>{item.name}</ItemData>
+                <ItemData>
+                  <div>
+                    {item.price}円
+                    {editable ? (
+                      <div>
+                        <select
+                          defaultValue={item.amount}
+                          onChange={(event) => {
+                            onChangeCartItemAmount(item.id, Number(event.target.value));
+                          }}
+                        >
+                          {[...Array(10)].map((_, i) => (
+                            <option key={i} value={i + 1}>
+                              {i + 1}個
+                            </option>
+                          ))}
+                        </select>
+                        <DeleteButton
+                          onClick={() => {
+                            onDeleteCartItem(item.id);
+                          }}
+                        >
+                          削除
+                        </DeleteButton>
+                      </div>
+                    ) : (
+                      item.amount
+                    )}
+                  </div>
+                </ItemData>
+                <ItemData>{item.price * item.amount}円</ItemData>
+              </ItemRow>
+            );
+          })}
+          {showTotal ? (
+            <tr>
+              <td colSpan={3}></td>
+              <TotalData>合計: {total}円</TotalData>
+            </tr>
+          ) : (
+            <tr />
+          )}
+        </tbody>
+      </Table>
+    </div>
   );
 };
