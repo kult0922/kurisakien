14aceacce6af8522e0c5f09a72af915a053f60d4
kult0922 <38486600+kult0922@users.noreply.github.com>
2021-12-20 19:57:50 +0900

    完了画面修正

--- src/pages/checkout/complete.tsx
+++ src/pages/checkout/complete.tsx
@@ -1,6 +1,7 @@
-import Link from 'next/link';
 import styled from '@emotion/styled';
 import { routing } from '../../constants/routing';
+import { Box } from '../../lib/styled';
+import { BasicLink } from '../../components/atoms/BasicLink';
 
 const Wrapper = styled.div({
   textAlign: 'center',
@@ -10,9 +11,11 @@ const Complete: React.FC = () => {
   return (
     <Wrapper>
       <h1>ご注文ありがとうございました</h1>
-      <Link href={routing.root}>
-        <a>ホームへ戻る</a>
-      </Link>
+      <Box>ご入力いただきましたメールアドレスに確認メールを送信しました。</Box>
+      詳しいお支払い手順につきましては再度メールを送ります。 今しばらくお待ち下さい。
+      <Box mt={20}>
+        <BasicLink path={routing.root} text={'ホームへ戻る'}></BasicLink>
+      </Box>
     </Wrapper>
   );
 };
