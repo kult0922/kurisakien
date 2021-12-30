f5e9fd0af4f6067a031eb57fae062a96fd5336b8
kult0922 <38486600+kult0922@users.noreply.github.com>
2021-12-30 23:21:43 +0900

    リファクタ

--- src/components/molecules/TabBar/index.tsx
+++ src/components/molecules/TabBar/index.tsx
@@ -10,13 +10,21 @@ export const TabBar: React.FC = () => {
   return (
     <>
       <Link href={routing.root} passHref>
-        <Tab hasOpened={'/' + pathname.split('/')[1] === routing.root}>ホーム</Tab>
+        <Tab hasOpened={pathname === routing.root}>ホーム</Tab>
       </Link>
       <Link href={routing.items.root} passHref>
-        <Tab hasOpened={'/' + pathname.split('/')[1] === routing.items.root}>注文</Tab>
+        <Tab
+          hasOpened={
+            pathname === routing.items.root ||
+            pathname === routing.items.id ||
+            pathname === routing.items.gifts.id
+          }
+        >
+          注文
+        </Tab>
       </Link>
       <Link href={routing.cart.root} passHref>
-        <Tab hasOpened={'/' + pathname.split('/')[1] == routing.cart.root}>
+        <Tab hasOpened={pathname === routing.cart.root}>
           <ShoppingCart width={18} height={18} />
         </Tab>
       </Link>
--- src/constants/routing/index.ts
+++ src/constants/routing/index.ts
@@ -1,5 +1,5 @@
 const items = '/items';
-const gifts = '/gifts';
+// const gifts = '/items/gifts';
 const news = '/news';
 const cart = '/cart';
 const checkout = '/checkout';
@@ -10,10 +10,9 @@ export const routing = {
   items: {
     root: items,
     id: items + '/[id]',
-  },
-  gifts: {
-    root: gifts,
-    id: gifts + '/[id]',
+    gifts: {
+      id: items + '/gifts/[id]',
+    },
   },
   news: {
     root: news,
