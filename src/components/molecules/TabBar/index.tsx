2790fbb657b4d011d297fbfa8da45227c13b8fa1
kult0922 <38486600+kult0922@users.noreply.github.com>
2021-12-30 22:37:41 +0900

    page削除

--- src/components/molecules/TabBar/index.tsx
+++ src/components/molecules/TabBar/index.tsx
@@ -6,18 +6,17 @@ import { useRouter } from 'next/router';
 
 export const TabBar: React.FC = () => {
   const { pathname } = useRouter();
-  const page = '/' + pathname.split('/')[1];
 
   return (
     <>
       <Link href={routing.root} passHref>
-        <Tab hasOpened={page === routing.root}>ホーム</Tab>
+        <Tab hasOpened={'/' + pathname.split('/')[1] === routing.root}>ホーム</Tab>
       </Link>
       <Link href={routing.items.root} passHref>
-        <Tab hasOpened={page === routing.items.root}>注文</Tab>
+        <Tab hasOpened={'/' + pathname.split('/')[1] === routing.items.root}>注文</Tab>
       </Link>
       <Link href={routing.cart.root} passHref>
-        <Tab hasOpened={page == routing.cart.root}>
+        <Tab hasOpened={'/' + pathname.split('/')[1] == routing.cart.root}>
           <ShoppingCart width={18} height={18} />
         </Tab>
       </Link>
