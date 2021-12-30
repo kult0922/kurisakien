2870a973106fe64401c22cd47c9ee200d5fb5a39
kult0922 <38486600+kult0922@users.noreply.github.com>
2021-12-30 21:36:42 +0900

    下線を保つように修正

--- src/components/atoms/Tab/index.ts
+++ src/components/atoms/Tab/index.ts
@@ -1,27 +1,29 @@
 import styled from '@emotion/styled';
 
-export const Tab = styled.a<{ bottom?: number | string }>(({ bottom = -4 }) => ({
-  textAlign: 'center',
-  position: 'relative',
-  display: 'inline-block',
-  color: '#1b1b1b',
-  textDecoration: 'none',
-  width: 60,
-  padding: '10px 20px 0 20px',
-  '&:after': {
-    position: 'absolute',
-    bottom,
-    left: '10%',
-    content: "''",
-    width: '80%',
-    height: '2px',
-    background: '#333',
-    transform: 'scale(0, 1)',
-    transformOrigin: 'right top',
-    transition: 'transform 0.2s',
-  },
-  '&:hover:after': {
-    transformOrigin: 'left top',
-    transform: 'scale(1, 1)',
-  },
-}));
+export const Tab = styled.a<{ bottom?: number | string; isSelect?: boolean }>(
+  ({ bottom = -4, isSelect = false }) => ({
+    textAlign: 'center',
+    position: 'relative',
+    display: 'inline-block',
+    color: '#1b1b1b',
+    textDecoration: 'none',
+    width: 60,
+    padding: '10px 20px 0 20px',
+    '&:after': {
+      position: 'absolute',
+      bottom,
+      left: '10%',
+      content: "''",
+      width: '80%',
+      height: '2px',
+      background: '#333',
+      transform: isSelect ? 'scale(1, 1)' : 'scale(0, 1)',
+      transformOrigin: 'right top',
+      transition: 'transform 0.2s',
+    },
+    '&:hover:after': {
+      transformOrigin: 'left top',
+      transform: 'scale(1, 1)',
+    },
+  }),
+);
--- src/components/molecules/TabBar/index.tsx
+++ src/components/molecules/TabBar/index.tsx
@@ -2,18 +2,22 @@ import Link from 'next/link';
 import { Tab } from '~/components/atoms/Tab';
 import { ShoppingCart } from '~/components/Icons/ShoppingCart';
 import { routing } from '~/constants/routing';
+import { useRouter } from 'next/router';
 
 export const TabBar: React.FC = () => {
+  const router = useRouter();
+  const page = '/' + router.pathname.split('/')[1];
+
   return (
     <>
       <Link href={routing.root} passHref>
-        <Tab>ホーム</Tab>
+        <Tab isSelect={page === routing.root}>ホーム</Tab>
       </Link>
       <Link href={routing.items.root} passHref>
-        <Tab>注文</Tab>
+        <Tab isSelect={page === routing.items.root}>注文</Tab>
       </Link>
       <Link href={routing.cart.root} passHref>
-        <Tab bottom="-7px">
+        <Tab bottom="-4px" isSelect={page == routing.cart.root}>
           <ShoppingCart width={18} height={18} />
         </Tab>
       </Link>
